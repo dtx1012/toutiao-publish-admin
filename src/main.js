c2e078c66a8577d935c0d753c06e2a71ff8116e7
迪天祥 <1426155410@qq.com>
2021-08-21 21:37:49 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
-//通过render方法把App根组件渲染到 #app 入口节点
+/* 通过render方法把App根组件渲染到 #app 入口节点 */
 new Vue({
   router,
   render: h => h(App)
