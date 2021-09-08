c0695ebead829f67bae580d3509b58be435813e9
迪天祥 <1426155410@qq.com>
2021-09-08 19:02:43 +0800

    素材管理

--- src/api/image.js
+++ src/api/image.js
@@ -9,9 +9,37 @@ import request from '@/utils/request'
 export const uploadImage = data => {
   return request({
     methods: 'POST',
-    url:'/mp/v1_0/user/images',
+    url: '/mp/v1_0/user/images',
     //一般文件上传的接口都需要把请求头中的Content-Type设置为multipart/form-data,但是我们使用axios上传文件的话不需要手动设置，你只要给data一个FormData对象即可。
     //new FormData()
     data
+  })
+}
+
+/* 获取素材列表 */
+export const getImages = params => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/images',
+    params
+  })
+}
+
+/* 收藏素材列表 */
+export const collectImage = (imageID, collect) => {
+  return request({
+    method: 'PUT',
+    url: `/mp/v1_0/user/images/${imageID}`,
+    data: {
+      collect
+    }
+  })
+}
+
+/* 删除素材 */
+export const deleteImage = (imageID) => {
+  return request({
+    method: 'DELETE',
+    url: `/mp/v1_0/user/images/${imageID}`,
   })
 }
